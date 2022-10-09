17e7b8565cb19a9055cefd9263727863e43d12fe
asifdzaki93 <81837937+asifdzaki93@users.noreply.github.com>
2022-10-09 11:25:35 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,3 @@
 <?php 
-    $koneksi = mysqli_connect("localhost", "root", "" ,"hs_keuanganbaru");
+    $koneksi = mysqli_connect("103.142.21.130", "asiftech_ruas", "RuasDigital123" ,"asiftech_ruas");
 ?>
